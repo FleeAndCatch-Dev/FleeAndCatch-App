54d26cd6c1e3a239a043c2610f4ed93de48b9593
ThunderSL94 <SL-Thunder@gmx.de>
2016-12-27 15:43:53 +0100

    ShowRobot - Add motion lib - Example for motion

--- FleeAndCatch-App/FleeAndCatch-App/FleeAndCatch_App/pages/content/control/Control.xaml.cs
+++ FleeAndCatch-App/FleeAndCatch-App/FleeAndCatch_App/pages/content/control/Control.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DeviceMotion.Plugin;
+using DeviceMotion.Plugin.Abstractions;
+using System.Diagnostics;
 using Xamarin.Forms;
 using Robots;
 
@@ -24,6 +26,71 @@ namespace FleeAndCatch_App.pages.content.control
         {
             base.OnAppearing();
             Title = Title + " " + Convert.ToString(robot.Identification.Id);
+
+            CrossDeviceMotion.Current.Start(MotionSensorType.Accelerometer, MotionSensorDelay.Ui);
+            CrossDeviceMotion.Current.SensorValueChanged += (s, a) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    switch (a.SensorType)
+                    {
+                        case MotionSensorType.Accelerometer:
+
+                            LX.Text = ((MotionVector)a.Value).X.ToString("F");
+                            LY.Text = ((MotionVector)a.Value).Y.ToString("F");
+                            LZ.Text = ((MotionVector)a.Value).Z.ToString("F");
+                            break;
+                        case MotionSensorType.Gyroscope:
+                            break;
+                        case MotionSensorType.Magnetometer:
+                            break;
+                        case MotionSensorType.Compass:
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+
+                    var x = Convert.ToDouble(((MotionVector)a.Value).X.ToString("F"));
+                    var y = Convert.ToDouble(((MotionVector)a.Value).Y.ToString("F"));
+                    var z = Convert.ToDouble(((MotionVector)a.Value).Z.ToString("F"));
+
+                    if (x <= 0.2 && x >= -0.2)
+                    {
+                        //Gerade aus
+                        if (y >= 0.1)
+                        {
+                            LResult.Text = "Langsamer";
+                        }
+                        else if (y <= -0.1)
+                        {
+                            LResult.Text = "Schneller";
+                        }
+                        else
+                        {
+                            LResult.Text = "Nichts";
+                        }
+                    }
+                    else
+                    {
+                        if (x >= 0.1)
+                        {
+                            LResult.Text = "Rechts";
+                        }
+                        else if (x <= -0.1)
+                        {
+                            LResult.Text = "Links";
+                        }
+                    }
+                });
+                    
+            };
+        }
+
+        protected override void OnDisappearing()
+        {
+            CrossDeviceMotion.Current.Stop(MotionSensorType.Accelerometer);
+
+            base.OnDisappearing();
         }
     }
 }
