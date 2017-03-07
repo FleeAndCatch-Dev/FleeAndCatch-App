e2b9ed1f9793d71111d4c5ae89657bcfd3be8410
Simon Lang <SL-Thunder@gmx.de>
2017-03-07 16:27:30 +0100

    Add new release

--- FleeAndCatch-App/FleeAndCatch-App/FleeAndCatch_App/Pages/ControlPage.xaml.cs
+++ FleeAndCatch-App/FleeAndCatch-App/FleeAndCatch_App/Pages/ControlPage.xaml.cs
@@ -5,9 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace FleeAndCatch_App.Pages
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ControlPage : ContentPage
     {
         public ControlPage()
